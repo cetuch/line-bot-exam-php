b911d24f1444f3d4c85a7a35dbcb9b874a09e574
cetuch <53572644+cetuch@users.noreply.github.com>
2019-08-04 23:14:30 +0700

    Update line_rss_bangkokbiznews_today.php

--- line_rss_bangkokbiznews_today.php
+++ line_rss_bangkokbiznews_today.php
@@ -9,11 +9,12 @@ curl_close($ch);
  
 $xml = new SimpleXmlElement($contents);
 //for($i=0; $i<count($xml->channel->item); $i++){
-	for($i=0; $i<10; $i++){
+	for($i=0; $i<5; $i++){
 $url = $xml->channel->item[$i]->link;
 $title = $xml->channel->item[$i]->title;
 $description = $xml->channel->item[$i]->description;
-$news .= $title.”\n”.$url.”\n\n”;
+$news .= $title."\n".$url."\n\n";
+
 }
 /////////////////////////////////////
 //token 
