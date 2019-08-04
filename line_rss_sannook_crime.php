07de6036aebaa9214a23df6a5012de74b64e0dbd
cetuch <53572644+cetuch@users.noreply.github.com>
2019-08-04 22:37:02 +0700

    Add files via upload

---
+++ line_rss_sannook_crime.php
@@ -0,0 +1,52 @@
+<?php
+
+/////////////////// ดึงข่าว ////////////////
+$ch = curl_init('http://rssfeeds.sanook.com/rss/feeds/sanook/news.crime.xml');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+$contents = curl_exec($ch);
+curl_close($ch);
+ 
+$xml = new SimpleXmlElement($contents);
+//for($i=0; $i<count($xml->channel->item); $i++){
+	for($i=0; $i<5; $i++){
+$url = $xml->channel->item[$i]->link;
+$title = $xml->channel->item[$i]->title;
+$description = $xml->channel->item[$i]->description;
+$pubDate = $xml->channel->item[$i]->pubDate;
+$news .= $title."\n".$url."\n".$pubDate."\n\n"; 
+
+}
+/////////////////////////////////////
+//token 
+$token_line = "vzVGuPv8uJUJDwEmjcfc3wXpWe2oFD6lX7Fr279uhjp";
+
+$message = $_REQUEST['message'];
+$chOne = curl_init(); 
+curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+// SSL USE 
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
+//POST 
+curl_setopt( $chOne, CURLOPT_POST, 1); 
+// Message 
+curl_setopt( $chOne, CURLOPT_POSTFIELDS, $message); 
+//ถ้าต้องการใส่รุป ให้ใส่ 2 parameter imageThumbnail และimageFullsize
+curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=$news"); 
+// follow redirects 
+curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1); 
+//ADD header array 
+$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$token_line.'', ); 
+curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
+//RETURN 
+curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
+$result = curl_exec( $chOne ); 
+//Check error 
+if(curl_error($chOne)) { echo 'error:' . curl_error($chOne); } 
+else { $result_ = json_decode($result, true); 
+echo "status : ".$result_['status']; echo "message : ". $result_['message']; } 
+//Close connect 
+curl_close( $chOne ); 
+
+
+?>
