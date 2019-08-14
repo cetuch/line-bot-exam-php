650e1fb2cb7c06020ae7188072aae6f87e6275fd
cetuch <53572644+cetuch@users.noreply.github.com>
2019-08-14 15:45:03 +0700

    Update picsech.php

--- picsech.php
+++ picsech.php
@@ -1,62 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>ค้นหารูปภาพ</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    /* Set height of the grid so .sidenav can be 100% (adjust if needed) */
+    .row.content {height: 1500px}
+    
+    /* Set gray background color and 100% height */
+    .sidenav {
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+    
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+    
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+      .sidenav {
+        height: auto;
+        padding: 15px;
+      }
+      .row.content {height: auto;} 
+    }
+  </style>
+</head>
+<body>
+<div class="container-fluid">
+  <div class="row content">
+  <form class="form-horizontal" method="post">
+  <fieldset>
+    <legend>Line Notify </legend>
+    <div class="form-group">
+      <label for="inputimage" class="col-lg-2 control-label">ใส่เลข 13 หลัก</label>
+      <div class="col-lg-10">
+        <input type="text" class="form-control" id="inputimage" name="inputimage" placeholder="Photo URL">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="textArea" class="col-lg-2 control-label">ข้อความเพิ่มเติม</label>
+      <div class="col-lg-10">
+        <textarea class="form-control" rows="3" id="textArea"  name="textArea"></textarea>
+        <span class="help-block"> กลุ่มสลัม </span>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-lg-10 col-lg-offset-2">
+        <button type="reset" class="btn btn-default">Cancel</button>
+        <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+      </div>
+    </div>
+  </fieldset>
+</form>
 <?php 
-	/*Get Data From POST Http Request*/
-	$datas = file_get_contents('php://input');
-	/*Decode Json From LINE Data Body*/
-	$deCode = json_decode($datas,true);
-	file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
-	$replyToken = $deCode['events'][0]['replyToken'];
-	$messages = [];
-	$messages['replyToken'] = $replyToken;
-	$messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");
-	$encodeJson = json_encode($messages);
-	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
-  	$LINEDatas['token'] = "9EQDL+a4qHdYrNxdDPC/eExmvMrLuaXSJuo6ieNPkFgkJlqeQr2cBJ9+OrPqi9hScSvWQ8EfByd7LFkSXVfM3Td0daGZ9wfmtHnkdU5ETGzDFm3JOiLLj8L17RCdon7qOa2fW8KmugttR5fE+lF1TgdB04t89/1O/w1cDnyilFU=";
-  	$results = sentMessage($encodeJson,$LINEDatas);
-	/*Return HTTP Request 200*/
-	http_response_code(200);
-	function getFormatTextMessage($text)
-	{
-		$datas = ["รูป"," "];
-		$datas['type'] = 'text';
-        $datas['text'] = $id;
-        $inputimage =  trim('inputimage');
-        $url = 'http://vpn.idms.pw:9977/polis/imagebyte?id='.$id;
-		return $datas,$url;
-	}
-	function sentMessage($encodeJson,$datas,$url)
-	{
-		$datasReturn = [];
-		$curl = curl_init();
-		curl_setopt_array($curl, array(
-		  CURLOPT_URL => $datas['url'],
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_ENCODING => "",
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 30,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => "POST",
-		  CURLOPT_POSTFIELDS => $encodeJson["message=$inputimage&imageFullsize=$inputimage"],
-		  CURLOPT_HTTPHEADER => array(
-		    "authorization: Bearer ".$datas['token'],
-		    "cache-control: no-cache",
-		    "content-type: application/json; charset=UTF-8",
-		  ),
-		));
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
-		curl_close($curl);
-		if ($err) {
-		    $datasReturn['result'] = 'E';
-		    $datasReturn['message'] = $err;
-		} else {
-		    if($response == "{}"){
-			$datasReturn['result'] = 'S';
-			$datasReturn['message'] = 'Success';
-		    }else{
-			$datasReturn['result'] = 'E';
-			$datasReturn['message'] = $response;
-		    }
-		}
-		return $datasReturn;
-	}
+if ($_POST) { 
+
+//Setting
+$lineapi = "vE1y2Cj7TrCg8b2QE5QTK5puWbvyYsRfWi5QEt7pCl2";  //โทเค่น line
+
+$mms =  trim($_POST['textArea']);
+$inputimage =  trim($_POST['inputimage']);
+$inputimage = 'http://vpn.idms.pw:9977/polis/imagebyte?id=' . $input;
+   
+   
+date_default_timezone_set("Asia/Bangkok");
+//line Send
+
+$chOne = curl_init(); 
+curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+// SSL USE 
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
+//POST 
+curl_setopt( $chOne, CURLOPT_POST, 1); 
+// Message 
+curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=$mms&imageThumbnail=$inputimage&imageFullsize=$inputimage"); 
+// follow redirects 
+curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1); 
+//ADD header array 
+$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$lineapi.'', ); 
+curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
+//RETURN 
+curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
+$result = curl_exec( $chOne ); 
+//Check error 
+if(curl_error($chOne)) { echo 'error:' . curl_error($chOne); } 
+else { $result_ = json_decode($result, true); 
+echo "status : ".$result_['status']; echo "message : ". $result_['message']; } 
+//Close connect 
+curl_close( $chOne );      
+}
 ?>
+</div>
+</div>
+</body>
+</html>
